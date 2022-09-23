31bbe66ce3f7d6cfd9e559a62b248e212ce2852c
Clare Dang <clare@pangenomic.com>
2022-09-23 01:42:34 -0700

    send message from tunnel to client successfully

--- tunnel.c
+++ tunnel.c
@@ -57,40 +57,61 @@ int main(int argc, char **argv)
     listen(listenfd, LISTENQ);
 
     // Connect to server
-    for ( ; ; ) {
-        len = sizeof(cliaddr);
+    len = sizeof(cliaddr);
 
-        if (connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &len) >= 0) {
-        printf("Connection from %s, port %d\n",inet_ntop(AF_INET, &cliaddr.sin_addr, buff, sizeof(buff)), ntohs(cliaddr.sin_port));
-        break;
-        }
+    connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &len);
+    printf("Connection from %s, port %d\n",inet_ntop(AF_INET, &cliaddr.sin_addr, buff, sizeof(buff)), ntohs(cliaddr.sin_port));
 
-        close(connfd);
-    }
 
     // ========= 3. Establish connection to server ==== ///
-        if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-            printf("socket error\n");
+    if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("socket error\n");
+        exit(1);
+    }
+
+    bzero(&dest_addr, sizeof(dest_addr));
+    dest_addr.sin_family = AF_INET;
+    // Change argv to int 
+    // char* p;
+    // int argv_to_int = strtol(argv[2], &p, 10);
+    // servaddr.sin_addr.s_addr = inet_addr[]
+    inet_aton("10.0.0.83", &dest_addr.sin_addr.s_addr); // server_ip
+    dest_addr.sin_port = htons(4444); // server_port
+
+    // if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
+    //     printf("inet_pton error for %s\n", argv[1]);
+    //     exit(1);
+    // }
+
+    if (connect(sockfd, (struct sockaddr *) &dest_addr, sizeof(dest_addr)) < 0) {
+        printf("connect error\n");
+        exit(1);
+    }
+
+
+    // Read message from server
+    while ( (n = read(sockfd, recvline, MAXLINE)) > 0) {
+        recvline[n] = 0;        /* null terminate */
+        if (fputs(recvline, stdout) == EOF) {
+            printf("fputs error\n");
             exit(1);
         }
+        printf("Tunnel: Read from server successfully\n");
+        // Send message to client
+        struct message msg;
+        ticks = time(NULL);
+        snprintf(msg.currtime, sizeof(msg.currtime), "%.24s\r\n", ctime(&ticks));
+        msg.timelen = sizeof(msg.currtime);
+        write(connfd, msg.currtime, msg.timelen);
+        printf("Sending response: %s", msg.currtime);
 
-        bzero(&dest_addr, sizeof(dest_addr));
-        dest_addr.sin_family = AF_INET;
-        // Change argv to int 
-        // char* p;
-        // int argv_to_int = strtol(argv[2], &p, 10);
-        // servaddr.sin_addr.s_addr = inet_addr[]
-        inet_aton("10.0.0.83", &dest_addr.sin_addr.s_addr); // server_ip
-        dest_addr.sin_port = htons(4444); // server_port
-
-        // if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
-        //     printf("inet_pton error for %s\n", argv[1]);
-        //     exit(1);
-        // }
-
-        if (connect(sockfd, (struct sockaddr *) &dest_addr, sizeof(dest_addr)) < 0) {
-            printf("connect error\n");
-            exit(1);
-        }
+        exit(1);
+    }
+    if (n < 0) {
+        printf("read error\n");
+        exit(1);
+    }
+   
+    close(connfd);
 }
 
